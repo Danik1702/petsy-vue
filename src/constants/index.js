558c1104961595b85a8212507872aad4ff60067a
BH21613 <danyil.kichalo@macys.com>
2022-07-29 15:27:42 +0300

    add page limit constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,10 @@
 export const ROUTES = {
   home: 'Home',
   breeds: 'Breeds',
+  breedDetails: 'BreedDetails',
   careCheck: 'CareCheck',
   comparison: 'Comparison',
   filter: 'Filter',
 }
+
+export const PETS_LIST_LIMIT = 10
