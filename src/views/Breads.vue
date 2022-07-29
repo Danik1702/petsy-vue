91f1f44ec2c445e09eade60cc28ac5ae1124c726
BH21613 <danyil.kichalo@macys.com>
2022-07-29 15:28:32 +0300

    remove unnesessary components

--- src/views/Breads.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="home">Breads page</div>
-</template>
-
-<script>
-export default {
-  name: 'BreadsView',
-}
-</script>
--- src/views/Filter.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="home">Filter page</div>
-</template>
-
-<script>
-export default {
-  name: 'FilterView',
-}
-</script>
