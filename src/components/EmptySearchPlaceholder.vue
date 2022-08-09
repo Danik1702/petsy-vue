78f564e1a8acb31e5325c49f06863f5f1e67d862
BH21613 <danyil.kichalo@macys.com>
2022-08-09 14:35:37 +0300

    implement the empty search component

---
+++ src/components/EmptySearchPlaceholder.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="empty-search">
+    <img
+      src="@/assets/img/empty-search.jpeg"
+      class="empty-search__image"
+      alt="empty-search"
+    />
+    <h3 class="empty-search__text">{{ text }}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'EmptySearchPlaceholder',
+  props: {
+    text: {
+      type: String,
+      default: 'There is no results..',
+    },
+  },
+}
+</script>
+
+<style scodep lang="scss">
+.empty-search {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .empty-search__image {
+    max-width: 100%;
+    width: 250px;
+    height: 250px;
+  }
+
+  .empty-search__text {
+    font-size: 26px;
+    line-height: 30px;
+    font-weight: normal;
+  }
+}
+</style>
